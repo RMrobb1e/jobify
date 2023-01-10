dd7627505b6941b5603a87faa0bf44cee4137284
Robbie Mariano <robbiemariano126@gmail.com>
2023-01-10 21:18:04 +0800

    feat: Add middlewares

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
 const app = express();
+
+// middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
