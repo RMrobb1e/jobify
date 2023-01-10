f29cefde0a15aa5c9bd4a96e16556f50ca0ea86a
Robbie Mariano <robbiemariano126@gmail.com>
2023-01-10 21:39:59 +0800

    feat: Setup mongo

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 const app = express();
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 // middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
+import connectDB from './db/connect.js';
 
 app.get('/', (req, res) => {
   res.send('Welcome');
@@ -14,6 +18,15 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
