bc4486d0a6f41cd3a7b457b67834e6db48c9247a
Robbie Mariano <robbiemariano126@gmail.com>
2023-01-11 20:14:52 +0800

    feat: Add error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,20 @@
-import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError } from '../errors/index.js';
+import User from '../models/User.js';
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,25 @@
 import { StatusCodes } from 'http-status-codes';
 
-const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+const errorHandlerMiddleware = (err, _req, res, _next) => {
+  console.log(err.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: 'Something went wrong, please try again later',
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, please try again later',
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+  }
+
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
